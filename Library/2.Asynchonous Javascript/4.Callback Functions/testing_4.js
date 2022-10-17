2235193c74cf3c461b8728082ca6985fb07fd593
Le Bris Corentin <corentin.lebris@hotmail.fr>
2022-10-17 15:54:49 +0200

    finish tuto 4

--- Library/2.Asynchonous Javascript/4.Callback Functions/testing_4.js
+++ Library/2.Asynchonous Javascript/4.Callback Functions/testing_4.js
@@ -1,17 +1,26 @@
-const getRequest = () => {
+const getRequest = (callback) => {
   const request = new XMLHttpRequest();
 
   request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
   request.send();
 
-  request.addEventListener('readystatechange',() =>{
-    if(request.readyState === 4 && request.status === 200){
-      console.log(request.responseText)
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status === 200){
+      callback(undefined, request.responseText)
     } else if(request.readyState === 4){
-      console.log(`ERROR !!, ${request.status}`)
+      callback('la data ne peut pas être lue', undefined)
     }
   });
 
 }
 
-getRequest();
+const handleResponse = (err, data) => {
+  if (err) {
+    console.log(err)
+  } else {
+  console.log(data)
+  }
+
+}
+
+getRequest(handleResponse);
