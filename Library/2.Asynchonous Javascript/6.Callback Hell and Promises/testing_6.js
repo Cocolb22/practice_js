bda5cd6f42bb4c211dde9a32c0cc63448a34cc32
Corentin Le Bris <corentin.lebris@hotmail.fr>
2022-10-18 20:07:38 +0200

    finish tuto 7

--- Library/2.Asynchonous Javascript/6.Callback Hell and Promises/testing_6.js
+++ Library/2.Asynchonous Javascript/6.Callback Hell and Promises/testing_6.js
@@ -17,7 +17,7 @@ const getRequest = (resource, callback) => {
   });
 
 }
-// nester les requetes
+
 const handleResponse = (err, data) => {
   if (err) {
     console.log(err)
@@ -26,10 +26,58 @@ const handleResponse = (err, data) => {
   }
 
 }
-
+// nester les requetes dans un callback
 getRequest('Json_files/test1.json', (err, data) => {
   console.log(data)
   getRequest('Json_files/test2.json', (err, data) => {
     console.log(data)
   });
 });
+
+
+// Promise
+const getSomething = () => {
+  return new Promise((resolve, reject) => {
+    // resolve('some data');
+    reject('some error;');
+  });
+};
+
+getSomething().then((data) =>{
+  console.log(data)
+}, (err) => {
+  console.log(err);
+});
+
+// promise avec .catch
+getSomething().then((data) => {
+  console.log(data)
+}).catch(err => {
+  console.log(err);
+});
+
+
+// Même requete mais avec une promise
+const getRequest2 = (resource) => {
+
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState === 4 && request.status === 200){
+        const data =JSON.parse(request.responseText)
+        resolve(data);
+      } else if(request.readyState === 4){
+        reject('erreur dans la récupération');
+      }
+    });
+    request.open('GET', resource);
+    request.send();
+  });
+}
+
+getRequest2('Json_files/test2.json').then(data => {
+  console.log('promise ok', data)
+}).catch(err => {
+  console.log ('mauvais retour', err)
+})
