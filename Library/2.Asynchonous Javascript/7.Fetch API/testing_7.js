4634925cbfd497d682091d1b8086add1c2f1b487
Corentin Le Bris <corentin.lebris@hotmail.fr>
2022-10-20 16:17:10 +0200

    finish tuto 11

--- Library/2.Asynchonous Javascript/7.Fetch API/testing_7.js
+++ Library/2.Asynchonous Javascript/7.Fetch API/testing_7.js
@@ -16,8 +16,16 @@ fetch("Json_files/test2.json").then((response)=>{
 const getResponse = async ()=>{
 
   const response = await fetch("Json_files/test3.json")
+
+  if(response.status !== 200){
+    throw new Error('impossible de récupérer la donnée');
+  };
+
   const data = await response.json();
+
   return data
 };
 
-getResponse().then(data => { console.log('bonne réponse', data)});
+getResponse()
+  .then(data => console.log('bonne réponse', data))
+  .catch(err => console.log('mauvaise réponse du serveur', err.message));
